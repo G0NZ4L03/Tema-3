87c44d39ca97b16b5a38ac0855ae4e3292b1c9d4
G0NZ4L03 <gonzalomarsau@gmail.com>
2025-10-28 13:58:20 +0100

    Reestructuración de los ejercicios de Tema 3 y creación del Ejercicio 2 de compresión de cadenas en JavaScript.

---
+++ Ejercicio 2/script.js
@@ -0,0 +1,16 @@
+/*
+Enunciado.
+2. Vamos utilizar nuestros conocimientos de manipulación de cadenas en javascript, para crear un algoritmo de compresión de cadenas. 
+El algoritmo consiste en sustituir rachas de caracteres repetidos por el número de repeticiones seguido del carácter. 
+El algoritmo sólo actúa con cadenas de caracteres del alfabeto, por lo que arrojará un mensaje de error en caso 
+de que la cadena contenga números. Si admite espacios, en cuyo caso se sustituirá el espacio por una interrogación "?".
+
+Ejemplos:
+Cadena de entrada: "holaaaa mundo" se transforma en "hol4a? mundo"
+Cadena de entrada: "aaabbcaaaa" se transforma en "3a2bc4a"
+
+ a). Crea una función de compresión que recibe la cadena y devuelve la cadena comprimida.
+ b). Crea una función de descompresión que recibe una cadena en formato comprimido y devuelve la cadena original.
+
+ Implementa una interfaz con dos botones que permita codificar y decodificar cadenas introducidas por el usuario.
+*/
