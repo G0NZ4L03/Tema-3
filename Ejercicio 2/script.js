870b6b2290447db1723d6971e173d1554dbf2c20
G0NZ4L03 <gonzalomarsau@gmail.com>
2025-10-30 00:55:06 +0100

    Ejercicio 2 a medias

--- Ejercicio 2/script.js
+++ Ejercicio 2/script.js
@@ -14,3 +14,46 @@ Cadena de entrada: "aaabbcaaaa" se transforma en "3a2bc4a"
 
  Implementa una interfaz con dos botones que permita codificar y decodificar cadenas introducidas por el usuario.
 */
+
+let botonCom = document.getElementById("botonCom").addEventListener("click", comprimir);
+let botonDescom = document.getElementById("botonDescom").addEventListener("click", descomprimir);
+
+let solucion = document.getElementById("solucion");
+
+function comprimir() {
+    //Cojo el valor de la cadena al momento de pulsar el boton
+    let cadena = document.getElementById("cadena").value; 
+    //Declaro el booleano para comprobar si hay numeros
+    let hayNumero = false;
+
+    //Recorro toda la cadena del input
+    for (let i = 0; i < cadena.length; i++) {
+        //Voy comprobando si el caracter actual es un numero para activar el booleano
+        if (cadena[i] >= '0' && cadena[i] <= '9') {
+            hayNumero = true;
+        } 
+    }
+
+            if (hayNumero) {    //Si hay algun numero mando el error
+                solucion.innerHTML = "Error: La cadena no debe contener números.";
+            }else {
+            let resultado = "";  
+            for (let i = 0; i < cadena.length; i++) {
+                let contador = 1;
+                let caracterActual = cadena[i];
+                while (caracterActual === cadena[i + 1]) {
+                    contador++;
+                    i++;
+                }
+                //ARREGLAR QUE NO PONGA EL 1 ANTES DE CARACTERES UNICOS
+                resultado = resultado + contador + caracterActual;
+            }
+            solucion.innerHTML = "El texto comprimido es: " + resultado;
+        }   
+
+}
+
+
+function descomprimir() {
+    solucion.innerHTML = "El texto descomprimido es: " + document.getElementById("cadena").value;
+}   
